e99684adaf227ebbc3f22309041d8a9ffce8bbad
Hans Klunder <hans.klunder@xs4all.nl>
2019-04-28 19:51:13 +0200

    workable demo

---
+++ dist/precache-manifest.6a9b02983a01e895288d1bd042fb83cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "faedf8d70cf299e74298",
+    "url": "/js/chunk-vendors.bf80e794.js"
+  },
+  {
+    "revision": "3132de945eedbd816a95",
+    "url": "/js/app.edb75429.js"
+  },
+  {
+    "revision": "1aca4beeeed2be9e17d46b9d54b55329",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faedf8d70cf299e74298",
+    "url": "/css/chunk-vendors.ed4dd371.css"
+  },
+  {
+    "revision": "3132de945eedbd816a95",
+    "url": "/css/app.bf81b2a6.css"
+  }
+];
