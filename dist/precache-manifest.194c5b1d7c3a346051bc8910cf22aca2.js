92f8d126b36a7e1dbbbd4aad4f8c59890ba955fc
Hans Klunder <hans.klunder@xs4all.nl>
2019-04-30 15:08:15 +0200

    updated icons

---
+++ dist/precache-manifest.194c5b1d7c3a346051bc8910cf22aca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "fb23559dc7f5151ca7f3",
+    "url": "js/chunk-vendors.0e0275bd.js"
+  },
+  {
+    "revision": "ce7e0e46a3374be4d4b6",
+    "url": "js/app.a16346ee.js"
+  },
+  {
+    "revision": "b7f24ff7ddb5b6c3223f91984972ac41",
+    "url": "index.html"
+  },
+  {
+    "revision": "fb23559dc7f5151ca7f3",
+    "url": "css/chunk-vendors.76c2e9de.css"
+  },
+  {
+    "revision": "ce7e0e46a3374be4d4b6",
+    "url": "css/app.b5d72d49.css"
+  }
+];
