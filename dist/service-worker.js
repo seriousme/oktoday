3cb3360d5790edb4ba93820601380762e7e29323
Hans Klunder <hans.klunder@xs4all.nl>
2019-10-27 18:58:53 +0100

    Updated build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.194c5b1d7c3a346051bc8910cf22aca2.js"
+  "precache-manifest.a33068ea252dc34e8bced3a80acf2492.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "oktoday"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "oktoday"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
